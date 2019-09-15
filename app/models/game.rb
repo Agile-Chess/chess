e45ce202b8b586c1aa2d2bf0dcf8606a9bc15576
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-09-15 00:28:50 +0000

    Add available scope to Games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_many :pieces
   has_many :users
+  scope :available, -> { where(black_player_id: nil) }
+  scope :available, -> { where(white_player_id: nil) }
 
   BOARD_SIZE = 8
   
