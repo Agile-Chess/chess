1d83d0c9a067a46df8370938b27383bcef14e9ae
mxwilbur <mxwilbur@outlook.com>
2019-09-23 20:01:49 +0000

    Add move valid logic to king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,24 @@
 class King < Piece
+
+
+  
+  # allowed base move for king 
+  def standard_move?(x, y)
+    x_chg = (x - x_position).abs
+    y_chg = (y - y_position).abs
+
+    (x_chg <= 1) && (y_chg <= 1)
+  end
+  
+  # placeholder for castling move option
+  def castling_move?(x, y)
+    false
+  end
+
+  # either standard_move or castling_move 
+  # must be true for move to be legal
+  def legal_move?(x, y)
+    standard_move?(x, y) || castling_move?(x, y)
+  end
+  
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,59 @@
 class Piece < ApplicationRecord
-  belongs_to :games
+  MIN_BOARD_SIZE = 0
+  MAX_BOARD_SIZE = 7
+
+  belongs_to :game
+  belongs_to :user
+
+  # determine if user of piece matches current turn
+  def user_owns_piece?
+    player_id == game.turn
+  end
+
+  # determine that piece moved
+  # a piece has to move each turn
+  def nil_move?(x, y)
+    x_position == x && y_position == y
+  end
+
+  # determine that move stays on the board
+  def move_on_board?(x, y)
+    (x <= MAX_BOARD_SIZE && x >= MIN_BOARD_SIZE) && 
+    (y <= MAX_BOARD_SIZE && y <= MIN_BOARD_SIZE)  
+  end
+
+  # determine that obstructing piece not yours
+  # determine that captured piece player_id not yours
+  # placeholder for process 'obstruction'
+  # using piece owner player_id rather than piece color to validate different owners of pieces
+  def capture_move?(x, y)
+    captured_piece = game.obstruction(x, y)
+    captured_piece && captured_piece.player_id != player_id
+  end
+
+  # placeholder - destination_obstruction your piece
+  def destination_obstruction?(x, y)
+    return false unless captured_move?(x, y)
+  end
+
+  # placeholder - obstructed_move blocks move
+  def obstructed_move?(x, y)
+    true
+  end
+
+
+
+  # determine that move is valid
+  # define legal_move via models/king.rb
+  # how to related king.rb test results on piece.rb?
+  def valid_move?(x, y)
+    return false unless user_owns_piece?
+    return false if nil_move?(x, y)
+    return false unless move_on_board?(x, y)
+    return false unless legal_move(x, y)
+    return false if obstructed_move?(x, y)
+    return false if destination_obstructed?(x, y)
+    true
+  end
+
 end
