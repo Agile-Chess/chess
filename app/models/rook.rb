6b53c908c3b5e285f58f6011653aa17afe6f5a59
Tracey <traceyphan95@gmail.com>
2019-09-25 17:04:50 +0000

    rook valid move updated

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,4 +3,25 @@ class Rook < Piece
 	  def html_code(color)
   	return (color == Piece::WHITE) ? '&#9814;' : '&#9820;'
   end
+
+    def legal_move?(x, y)
+    if x<0 or x>7
+      return false
+    is_moving_horizontal = horizontal_move x
+    is_moving_veritical = veritcal_move y
+    return is_moving_horizontal ^ is_moving_vertical
+  end
+
+  private
+
+  def horizontal_move?(x)
+    x_diff = (x_position - x).abs
+    x_diff != 0
+  end
+
+  def vertical_move?(y)
+    y_diff = (y_position - y).abs
+    y_diff != 0
+  end
+
 end
