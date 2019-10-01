ccfb7af08b197e08e8c80a646afc45401894d592
Ashley <ashjohnson318@gmail.com>
2019-10-01 16:17:36 +0000

    valid move for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,4 +3,13 @@ class Knight < Piece
 	  def html_code(color)
   	return (color == Piece::WHITE) ? '&#9816;' : '&#9822;'
   end
+
+  def valid_move?(x,y)
+
+  	x_position = x_position(x)
+  	y_position = y_position(y)
+
+  	(x_position == 1 && y_position == 2) || (x_position == 2 && y_position == 1)
+
+  end
 end
