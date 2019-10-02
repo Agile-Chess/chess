b030a45003d126e5e8c810c2afd1deacfbae4aa6
Wilson Horrell <wilsonhorrell@gmai.com>
2019-10-02 15:50:04 +0000

    Pulling master

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_26_202043) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_09_26_202043) do
     t.datetime "updated_at", null: false
     t.integer "turn"
     t.string "state"
-    t.index ["black_player_id", "white_player_id"], name: "index_pieces_on_black_and_white_player_id", unique: true
     t.index ["black_player_id"], name: "index_pieces_on_black_player_id"
     t.index ["white_player_id"], name: "index_pieces_on_white_player_id"
   end
