b61e075ccf640e12ba95dad7c482187ff1710aaf
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-10-02 02:22:19 +0000

    Add valid_move? For the Bishop2

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -5,9 +5,9 @@ class Bishop < Piece
   end
 
   def valid_move?(x_des, y_des)
-    if x_des < -7 && > 7
+    if x_des < 0 || x_des > 7
       return false
-    elsif y_des < -7 && > 7
+    else y_des < 0 || y_des > 7
       return false
     end
     x_diff = (x_position - x_des).abs
@@ -15,5 +15,5 @@ class Bishop < Piece
     y_diff = (y_position - y_des).abs
     y_diff != 0
   end
-  
+
 end
