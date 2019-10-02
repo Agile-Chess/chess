264a5baecbce900998ae4e79383f5952a8b5133d
Ashley <ashjohnson318@gmail.com>
2019-10-02 01:12:35 +0000

    move knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -4,12 +4,5 @@ class Knight < Piece
   	return (color == Piece::WHITE) ? '&#9816;' : '&#9822;'
   end
 
-  def valid_move?(x,y)
 
-  	x_position = x_position(x)
-  	y_position = y_position(y)
-
-  	(x_position == 1 && y_position == 2) || (x_position == 2 && y_position == 1)
-
-  end
 end
