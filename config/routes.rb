c4881a9ad6290586fa0d6bb66f9cd50716d42676
Wilson Horrell <wilsonhorrell@gmai.com>
2019-10-03 17:36:32 +0000

    I can't seem to get the pieces to drop.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :games do
+  resources :games 
   resources :pieces
-  resources :pieces
-    member do
-      put :join
-    end
-  end
+ 
+    
+  
 end
 
