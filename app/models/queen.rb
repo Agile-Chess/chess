a07e3fc9d6770591726838e82df9bc40ad1a9a72
Alif Momin <alifmomin1234567890@gmail.com>
2019-10-05 16:23:01 +0000

    Added Queen.rb Valid_Move

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,6 +1,14 @@
 class Queen < Piece
 
-	  def html_code(color)
+  def html_code(color)
   	return (color == Piece::WHITE) ? '&#9813;' : '&#9819;'
   end
+
+  def valid_move?(x_des, y_des)
+    return false if !within_bounds?(x_des, y_des)
+    (x_pos == x_des || y_pos == y_des || x_diff(x_des) == y_diff(y_des))
+  end
+
+
+
 end
