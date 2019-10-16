7c84cb896996d5389709e1be2e6645f8a5f7ed66
Tracey <traceyphan95@gmail.com>
2019-10-16 16:36:13 +0000

    castling complete

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,6 +1,4 @@
 class King < Piece
-
-  
   
   # allowed base move for king 
   def standard_move?(x, y)
@@ -13,7 +11,37 @@ class King < Piece
   # placeholder for castling move option
   def castling_move?(x, y)
     false
-    #write code for 
+    # Have pieces moved before?
+    if has_moved
+      return false
+    end
+    x_chg = (x - x_position).abs
+    y_chg = (y - y_position).abs
+    if y_chg != 0 or x_chg != 2 
+      return false
+    end
+    #which rook
+    if (x - x_position) > 0 
+      rook_x = 0
+    else
+      roox_x = 7
+    end
+    #query to pull down rook
+    rook = Piece.find_by(game_id: game_id, x_position: rook_x, y_position: y_position, type: "rook")
+    if not rook.present?
+      return false
+    end
+    if rook.has_moved
+      return false
+    end
+    # Is the path obstructed?
+    (([rook_x, x_position].min + 1)..([x_position, rook_x].max - 1)).each do |n|   
+     checked_piece = Piece.find_by(game_id: game_id, x_position: n, y_position: y_position)
+    if checked_piece.present?
+      return false
+    end
+    end
+    return true
   end
 
   # either standard_move or castling_move 
